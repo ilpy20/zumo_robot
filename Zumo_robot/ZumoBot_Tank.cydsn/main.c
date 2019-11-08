c3cb155bb4aa5c9f9f95a3a6691940bf527fb964
Ilya Pyshkin <19843442+ilpy20@users.noreply.github.com>
2019-11-08 13:32:59 +0200

    Edit main.c

--- Zumo_robot/ZumoBot_Tank.cydsn/main.c
+++ Zumo_robot/ZumoBot_Tank.cydsn/main.c
@@ -267,6 +267,30 @@ void zmain(void)
  }   
 #endif
 
+#if 0
+    void tankturn_left(uint8 speed_left, uint8 speed_right, int delay);
+    
+void zmain(void)
+{
+    Ultra_Start();
+    motor_start(0,0);
+    motor_forward(0,0);
+    
+    vTaskDelay(3000);
+    
+    while(true){
+    int distance = Ultra_GetDistance();
+    
+    if(distance <= 10 && distance !=0){
+        motor_backward(50,500);
+        Beep(150, 200);
+        motor_turn(150,150, 500);
+        motor_forward(0,0);
+     }
+    motor_turn(200,100,50);
+    }   
+}
+#endif
 
 #if 0
 //ultrasonic sensor//
